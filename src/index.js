0fda4c1fff38562dc3be5d3a70501b7e28cbc9c6
Nicksm98 <nicholas.sanmartin20@gmail.com>
2025-02-04 13:38:35 -0500

    bug fixing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import { createRoot } from "react-dom/client"; // Import createRoot
+import ReactDOM from 'react-dom';
 
-import App from "./App"; // Import your main App component
+import App from './App';
 
-import "./index.css"; // Import your CSS (if you have one)
+import "./index.css"; 
 
-// Get the root element from your HTML (usually <div id="root"></div>)
-
-const container = document.getElementById("root");
-
-// Create a root for your React app using createRoot
-
-const root = createRoot(container); // createRoot returns a Root object.
-
-// Render your App component inside the root
-
-root.render(
-  <div>
-    {" "}
-    {/* Use StrictMode in development for catching common bugs */}
-    <App />
-  </div>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
